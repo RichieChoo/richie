0206873f0373a88461b811ea1cc9583a8a6ea230
RichieChoo <richiechoo1993@gmail.com>
2020-09-05 23:08:00 +0800

    add defaultAlias

--- packages/npm-alias/lib/npm-alias.js
+++ packages/npm-alias/lib/npm-alias.js
@@ -2,48 +2,66 @@
 const package = require('../package.json');
 const spawn = require('child_process').spawn;
 const commander = require('commander');
-const { option } = require('commander');
+const os = require('os');
 
+const isRf = os.type() === 'Darwin' || os.type() === 'Linux';
 const program = new commander.Command();
 const defaultAlias = {
-	ni: 'npm install',
-	nu: 'npm update',
-	nup: 'npm run update',
-	nd: 'npm run dev',
-	ns: 'npm run serve',
-	nus: 'npm install && npm update && npm run serve',
-	nub: 'rm -rf node_modules/.cache && npm install && npm update && npm run build'
+    ni: 'npm install',
+    nu: 'npm update',
+    nup: 'npm run update',
+    nd: 'npm run dev',
+    ns: 'npm run serve',
+    rc: isRf ? 'rm -rf node_modules/.cache' : 'rimraf node_modules/.cache',
+    nus: 'npm install && npm update && npm run serve',
+    nub: 'npm install && npm update && npm run build'
 };
 
+const needClean = (key) => ['nus', 'nub'].includes(key);
+const getBash = alias => needClean(alias) ? defaultAlias.rc + ' && ' + defaultAlias[alias] : defaultAlias[alias];
 program
-	.version(package.version, `-v, version`)
-	.description('provide frequently used npm alias ')
-	.arguments('<alias> [options]')
-	.action((alias, options) => {
+    .version(package.version, `-v, version`)
+    .description('provide frequently used npm alias ')
+    .arguments('<alias> [options]')
+    .action((alias, options) => {
         if (defaultAlias[alias]) {
-            console.log(`Alias receive "${alias}" : ${defaultAlias[alias]}`)
-			const [ cmd, ...params ] = defaultAlias[alias].split(' ');
-			const itemSpawn = spawn(cmd, params, { detached: true, stdio: 'inherit' });
+            const bash = getBash(alias)
+            console.log(`npm-alias receive "${alias}" : ${bash}`);
+            const [cmd, ...params] = bash.split(' ');
+            const itemSpawn = spawn(cmd, params, { shell: true, detached: true, stdio: 'inherit' });
 
-			itemSpawn.stdout &&
-				itemSpawn.stdout.on('data', (data) => {
-					console.log(data.toString());
-				});
+            itemSpawn.stdout &&
+                itemSpawn.stdout.on('data', (data) => {
+                    console.log(data.toString());
+                });
 
-			itemSpawn.stderr &&
-				itemSpawn.stderr.on('data', (data) => {
-					console.error(data.toString());
-				});
+            itemSpawn.stderr &&
+                itemSpawn.stderr.on('data', (data) => {
+                    console.error(data.toString());
+                });
 
-			itemSpawn.on('close', (code) => {
-				if (code !== 0) {
-					console.log(`Alias "${alias}" : ${defaultAlias[alias]} exited with code ${code}`);
-				} else {
-					console.log(`Alias "${alias}" : ${defaultAlias[alias]} excuted complete!`);
-				}
-			});
-		} else {
-			console.log(`Alias "${alias}" is not exits !`);
-		}
-	});
+            itemSpawn.on('close', (code) => {
+                if (code !== 0) {
+                    console.log(`npm-alias "${alias}" : ${bash} exited with code ${code}`);
+                } else {
+                    console.log(`npm-alias "${alias}" : ${bash} excuted complete!`);
+                }
+            });
+        } else {
+            console.log(`npm-alias "${alias}" is not exits !`);
+        }
+    })
+    .on('--help', function () {
+        const str = Object.keys(defaultAlias)
+            .map((key) => {
+                if (key !== 'rc') {
+                    const itemStr = `  ${key}`.padEnd(15, ' ');
+                    const valStr = getBash(key);
+                    return itemStr + valStr + '\n';
+                }
+            })
+            .filter(Boolean)
+            .join('');
+        console.log(str);
+    });
 program.parse(process.argv);
