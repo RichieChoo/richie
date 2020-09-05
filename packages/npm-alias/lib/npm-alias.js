10106e6290fdcdbf37ff9d60cada645ac1b640fa
RichieChoo <richiechoo1993@gmail.com>
2020-09-05 15:17:09 +0800

    add ra

--- packages/npm-alias/lib/npm-alias.js
+++ packages/npm-alias/lib/npm-alias.js
@@ -1,1 +1,6 @@
 #!/usr/bin/env node
+
+const package = require('../package.json');
+const commander = require('commander');
+commander.version(package.version, `-v, --version`);
+commander.parse(process.argv);
