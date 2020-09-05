524a16c7e12d5b94226099c39600a4887a3640fc
RichieChoo <richiechoo1993@gmail.com>
2020-09-05 17:37:43 +0800

    add alias for npm-alias

--- packages/npm-alias/lib/npm-alias.js
+++ packages/npm-alias/lib/npm-alias.js
@@ -1,28 +1,49 @@
 #!/usr/bin/env node
-const alias = {
+const package = require('../package.json');
+const spawn = require('child_process').spawn;
+const commander = require('commander');
+const { option } = require('commander');
+
+const program = new commander.Command();
+const defaultAlias = {
 	ni: 'npm install',
 	nu: 'npm update',
 	nup: 'npm run update',
 	nd: 'npm run dev',
 	ns: 'npm run serve',
-	nus: 'rm -rf node_modules/.cache && npm install && npm update && npm run serve',
+	nus: 'npm install && npm update && npm run serve',
 	nub: 'rm -rf node_modules/.cache && npm install && npm update && npm run build'
 };
 
-const package = require('../package.json');
-const exec = require('child_process').exec;
-const commander = require('commander');
-commander
-	.version(package.version, `-v, --version`)
-	.option('-i,ni', alias.ni)
-	.option('-u,nu', alias.nu)
-	.option('-up,nup', alias.nup)
-	.option('-ud,nd', alias.nd)
-	.option('-us,nus', alias.nus)
-	.option('-ub,nub', alias.nd);
-commander.parse(process.argv);
-Object.entries(alias).forEach(([ key, val ]) => {
-	if (commander[key]) {
-		exec(val);
-	}
-});
+program
+	.version(package.version, `-v, version`)
+	.description('provide frequently used npm alias ')
+	.arguments('<alias> [options]')
+	.action((alias, options) => {
+        if (defaultAlias[alias]) {
+            console.log(`Alias receive "${alias}" : ${defaultAlias[alias]}`)
+			const [ cmd, ...params ] = defaultAlias[alias].split(' ');
+			const itemSpawn = spawn(cmd, params, { detached: true, stdio: 'inherit' });
+
+			itemSpawn.stdout &&
+				itemSpawn.stdout.on('data', (data) => {
+					console.log(data.toString());
+				});
+
+			itemSpawn.stderr &&
+				itemSpawn.stderr.on('data', (data) => {
+					console.error(data.toString());
+				});
+
+			itemSpawn.on('close', (code) => {
+				if (code !== 0) {
+					console.log(`Alias "${alias}" : ${defaultAlias[alias]} exited with code ${code}`);
+				} else {
+					console.log(`Alias "${alias}" : ${defaultAlias[alias]} excuted complete!`);
+				}
+			});
+		} else {
+			console.log(`Alias "${alias}" is not exits !`);
+		}
+	});
+program.parse(process.argv);
